a3c078d4440cb2489f5df8d459edc90146af69a4
Tanishkdesignoweb <tanishk.designoweb@gmail.com>
2025-04-07 11:46:01 +0530

    Update Mainlayout.jsx

--- src/layout/Mainlayout.jsx
+++ src/layout/Mainlayout.jsx
@@ -7,7 +7,7 @@ function Mainlayout() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[250px_1fr] h-[100vh] max-w-[1860px] mx-auto">
       <Sidebar />
-      <div className="p-6">
+      <div className="p-6 h-full overflow-y-auto">
         <Outlet />
       </div>
     </div>
