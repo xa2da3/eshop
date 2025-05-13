dc10a55650a2b65af61ebde8c0c0b3d9b75a2a1d
akshu <in.akr.2000@gmail.com>
2025-05-13 21:37:43 +0530

    nav login button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Lex Arbitra</div>
+      <div className={styles.logo} onClick={()=>navigate('/')}>Lex Arbitra</div>
       <div className={styles.navList}>
         <div className={styles.navItems}>Features</div>
         <div className={styles.navItems}>Pricing</div>
         <div className={styles.navItems}>About</div>
         <div className={styles.navItems}>Products</div>
-        <div className={styles.navItemButton}>Log In</div>
+        <div className={styles.navItemButton} onClick={()=>navigate('/login')}>Log In</div>
       </div>
     </div>
   );
