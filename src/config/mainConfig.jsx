a33f567365d36c3ee4527b8a7204320554da1d05
akshu <in.akr.2000@gmail.com>
2025-05-16 07:52:58 -0700

    Refactor codebase to align with new architecture

--- src/config/mainConfig.jsx
+++
@@ -1,7 +0,0 @@
-const config = {
-    // apiURL : "https://api.lexarbitra.info/api/v1",
-    apiURL : "https://model.lexarbitra.info",
-    // apiURL : "http://localhost:3000/api/v1",
-}
-
-export default config;
