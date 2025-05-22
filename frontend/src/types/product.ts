fd73b89266216f9efccbc1ee6a6d9a0b2fa38bf7
xa2da3 <adx.apr23@gmail.com>
2025-05-22 14:57:00 +0700

    Update product.ts

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,13 +1,13 @@
 export interface Product {
   id: number;
-  external_id: string;
-  variants: number;
-  synced: number;
   name: string;
   thumbnail_url: string;
   price: number;
-  size: string
-  retailPrice?: string;
+  size?: string
+  synced?: number;
+  variants?: number;
+  external_id?: string;
+  retailPrice?: number;
   category?: string;
   sync_variants?: number[];
 }
